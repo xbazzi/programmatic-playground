ec16a64e9c42ab41558861dcf43536bd11acb8ac
Xander Bazzi <alexanderbazzi@gmail.com>
2024-02-24 12:49:30 -0700

    Add tcp client and server programs.

---
+++ cpp/network/tcp_client.cpp
@@ -0,0 +1,96 @@
+/// Author:  Alexander Bazzi
+/// Description: Connects to a tcp server and sends a message over a socket
+
+#include <iostream>
+#include <cstring> // For memset on POSIX
+
+// Platform-specific includes and initialization
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "Ws2_32.lib")
+
+    typedef SOCKET SocketType;
+    #define CLOSESOCKET closesocket
+    #define PRINTLASTERROR std::cerr << "WSAGetLastError: " << WSAGetLastError() << std::endl;
+
+    void init_network() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "WSAStartup failed.\n";
+            exit(1);
+        }
+    }
+
+    void cleanup_network() {
+        WSACleanup();
+    }
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+
+    typedef int SocketType;
+    #define CLOSESOCKET close
+    #define PRINTLASTERROR perror("Error")
+
+    void init_network() {
+        // No initialization needed for POSIX
+    }
+
+    void cleanup_network() {
+        // No cleanup needed for POSIX
+    }
+#endif
+
+namespace tcp
+{
+
+// Define a portable read function
+ssize_t portable_read(SocketType socket, char *buffer, size_t length) {
+    #ifdef _WIN32
+    return recv(socket, buffer, length, 0);
+    #else
+    return read(socket, buffer, length);
+    #endif
+}
+
+int main() {
+    init_network();
+
+    SocketType sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        PRINTLASTERROR;
+        return 1;
+    }
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr)); // Ensure struct is empty
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(5000);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        std::cout << "\nInvalid address/ Address not supported \n";
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cout << "\nConnection Failed \n";
+        return -1;
+    }
+
+    const char* hello = "Hello from client";
+    send(sock, hello, strlen(hello), 0);
+    std::cout << "Hello message sent\n";
+
+    char buffer[1024] = {0};
+    portable_read(sock, buffer, 1024);
+    std::cout << "Server: " << buffer << std::endl;
+
+    CLOSESOCKET(sock);
+    cleanup_network();
+
+    return 0;
+}
+} // End of tcp namespace
---
+++ cpp/network/tcp_server.cpp
@@ -0,0 +1,113 @@
+/// Author:  Alexander Bazzi
+/// Description: Initializes a socket to listen for a tcp connection from a 
+/// client on the specified port.
+
+#include <iostream>
+#include <cstring> // For memset on POSIX
+
+// Platform-specific includes and initialization
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "Ws2_32.lib")
+
+    typedef SOCKET SocketType;
+    #define CLOSESOCKET closesocket
+    #define PRINTLASTERROR std::cerr << "WSAGetLastError: " << WSAGetLastError() << std::endl;
+
+    void init_network() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "WSAStartup failed.\n";
+            exit(1);
+        }
+    }
+
+    void cleanup_network() {
+        WSACleanup();
+    }
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+
+    typedef int SocketType;
+    #define CLOSESOCKET close
+    #define PRINTLASTERROR perror("Error")
+
+    void init_network() {
+        // No initialization needed for POSIX
+    }
+
+    void cleanup_network() {
+        // No cleanup needed for POSIX
+    }
+#endif
+namespace tcp
+{
+// Define a portable read function
+ssize_t portable_read(SocketType socket, char *buffer, size_t length) {
+    #ifdef _WIN32
+    return recv(socket, buffer, length, 0);
+    #else
+    return read(socket, buffer, length);
+    #endif
+}
+
+int main() {
+    init_network();
+
+    SocketType server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    const char *msg = "Hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        PRINTLASTERROR;
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(opt))) {
+        PRINTLASTERROR;
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; // or specific IP
+    address.sin_port = htons(8080);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        PRINTLASTERROR;
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        PRINTLASTERROR;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Listening on port 8080...\n";
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        PRINTLASTERROR;
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t valread = portable_read(new_socket, buffer, 1024);
+    std::cout << "Message from client: " << buffer << std::endl;
+    send(new_socket, msg, strlen(msg), 0);
+    std::cout << "Hello message sent\n";
+
+    CLOSESOCKET(new_socket);
+    CLOSESOCKET(server_fd);
+    cleanup_network();
+
+    return 0;
+}
+} // End of tcp namespace
