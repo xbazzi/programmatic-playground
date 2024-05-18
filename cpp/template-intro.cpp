f72e51a5c853b255b2d22ba68b9c802286723fa3
Xander Bazzi <codemaster@xbazzi.com>
2024-05-18 16:27:58 -0600

    Add template practice code.

---
+++ cpp/template-intro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template<typename T>
+const T& max(const T& a, const T& b) {
+    return a > b ? a : b;
+}
+
+int main() {
+    const int a = 0, b = 2;
+    std::cout << max<int>(a, b) << std::endl;
+    std::cout << max<long double>(4.56, 434.5) << std::endl;
+    std::cout << max<char>('a', 'b') << std::endl;
+    std::cout << max<bool>(true, false) << std::endl;
+    return 0;
+}
