d2b112807d9bb44c9871d504a60d52648d0656d2
Xander Bazzi <codemaster@xbazzi.com>
2024-05-19 16:12:36 -0600

    Add quicksort to Sorter class.

--- cpp/dsa/sorting/Sorter.cpp
+++ cpp/dsa/sorting/Sorter.cpp
@@ -4,7 +4,37 @@
 class Sorter {
     public:
 
-    static void insertionSort(std::vector<int> &vec) {
+    /// @brief  Print all elements in the vector.
+    /// @tparam T 
+    /// @param  vec 
+    template<typename T>
+    static void print(std::vector<T> &vec) {
+        for (int &el : vec) std::cout << el << " ";
+        std::cout << std::endl;
+    }
+
+    /// @brief  Calculate the smallest element in the vector
+    /// @tparam T 
+    /// @param  it Starting position iterator
+    /// @param  end Final position iterator
+    /// @return Smallest element in the range between iterators
+    template<typename T>
+    static typename std::vector<T>::iterator smallest(typename std::vector<T>::iterator it, typename std::vector<T>::iterator end) {
+        typename std::vector<T>::iterator smallest_it = it;
+        for (; it != end; it++) {
+            if (*it < *smallest_it) {
+                smallest_it = it;
+            }
+        }
+        return smallest_it;
+    }
+
+    /// @brief  Sort the vector using insertion sort.
+    /// @tparam T 
+    /// @param  vec 
+    /// @todo   Develop a solid understanding of this implementation.
+    template<typename T>
+    static void insertionSort(std::vector<T> &vec) {
         int n = vec.size();
         for (int i = 1; i < n; i++) {
             int key = vec[i];
@@ -17,25 +47,60 @@ class Sorter {
                 j = j - 1;
             }
             vec[j + 1] = key;
-            }
+        }
     }
 
-    static void print(std::vector<int> &vec) {
-        for (int &el : vec) std::cout << el << std::endl;
+    /// @brief  Sort the vector with selection sort.
+    /// @tparam T 
+    /// @param  vec 
+    template<typename T>
+    static void selectionSort(std::vector<T> &vec) {
+        for (auto currIt = vec.begin();currIt != vec.end(); currIt++) {
+            auto minIt = smallest<T>(currIt, vec.end());
+            if (minIt != currIt) std::iter_swap(minIt, currIt);
+        }
     }
+
+    template<typename T>
+    static std::vector<T> quickSort(std::vector<T> &vec) {
+        if (vec.size() < 2) return vec;
+        int n = vec.size();
+        std::vector<T> smaller;
+        std::vector<T> equal;
+        std::vector<T> larger;
+        int pivotValue = vec[0];
+        for (const T& item : vec) {
+            if (item < pivotValue) {
+                smaller.push_back(item);
+            } else if (item == pivotValue) {
+                equal.push_back(item);
+            } else {
+                larger.push_back(item);
+            }
+        }
+        smaller = quickSort(smaller);
+        larger = quickSort(larger);
+        
+        std::vector<T> sortedVec;
+        sortedVec.insert(sortedVec.end(), smaller.begin(), smaller.end());
+        sortedVec.insert(sortedVec.end(), equal.begin(), equal.end());
+        sortedVec.insert(sortedVec.end(), larger.begin(), larger.end());
+
+        return sortedVec;
+    }
+
+
 };
 
 int main() {
     std::vector<int> test;
-    test.push_back(9);
+    test.push_back(5);
     test.push_back(4);
+    test.push_back(3);
     test.push_back(2);
-    test.push_back(7);
     test.push_back(1);
     Sorter::print(test);
 
-    std::cout << std::endl;
-
-    Sorter::insertionSort(test);
+    test = Sorter::quickSort(test);
     Sorter::print(test);
 }
