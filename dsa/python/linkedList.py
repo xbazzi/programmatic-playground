b8a52bf2a87006272c10486b4e5537c2cdd668bf
Xander Bazzi <codemaster@xbazzi.com>
2024-07-09 21:25:59 -0600

    Start python LinkedList implementation.

---
+++ dsa/python/linkedList.py
@@ -0,0 +1,19 @@
+
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+class LinkedList:
+    def __init__(self, node):
+        self.node = node
+    
+    def print_list(self) -> None:
+
+
+
+def main():
+    pass
+        
+if __name__ == "__main__":
+    main()
