49442dba92fff6d44745336fb68e349ff3d2ebcc
xbazzi <codemaster@xbazzi.com>
2025-11-25 22:51:09 -0700

    unordered_map quick drive

---
+++ cpp/hashmaps.cc
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+int main()
+{
+    std::unordered_map<std::string, std::vector<std::string>> map;
+    map.insert({ "A", { "B", "C", "D" } });
+
+    return EXIT_SUCCESS;
+}
